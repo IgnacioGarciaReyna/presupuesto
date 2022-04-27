294bcec8af82fda4b644ca69ea23761d4e4661ac
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-26 21:34:13 -0300

    Se agregaron el onChange y onSubmit

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,17 +1,48 @@
-import React, { Fragment } from "react";
+//Como el state no pasa por todos los componentes lo definimos acá, es un poco mas local.
+import React, { Fragment, useState } from "react";
 
 const Pregunta = () => {
+  //Definimos el state
+  //Como es una cantidad, la iniciamos en cero.
+  const [cantidad, guardarCantidad] = useState(0);
+
+  //Función que lee el presupuesto
+  //Para acceder a los valores del evento usamos e.
+  const definirPresupuesto = (e) => {
+    //Si escribimos solo e.target.value nos devuelve un string, pero nosotros queremos un número, por eso usamos parseInt
+    guardarCantidad(parseInt(e.target.value), 10);
+  };
+
+  //Submit para definir el presupuesto
+  //Usamos e para poner el preventDefault y evitar que recargue la página y envie los datos a otro lado.
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+
+    //Validar
+
+    //Que pasa si pasa la validación?
+  };
+
   return (
     //Utilizamos un fragment porque vamos a retornar un form.
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
-      <form>
+      <form
+        //Agregamos el onSubmit
+        onSubmit={agregarPresupuesto}
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          //Agregamos el onChange para enterarnos cuando cambia el input.
+          onChange={definirPresupuesto}
         />
-        <input type="submit" className="button-primary u-full-width" value="Definir Presupuesto"/>
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir Presupuesto"
+        />
       </form>
     </Fragment>
   );
